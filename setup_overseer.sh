b6222b03b002e01508c0d49f2ae2474be900c13d
b1tray3r <aborgardt.public@gmail.com>
2023-01-21 13:30:18 +0100

    fix syntax

--- setup_overseer.sh
+++ setup_overseer.sh
@@ -2,10 +2,11 @@
 
 if [ -f "./.env" ]; then
     echo "Load environment variables from .env files"
+
     . .env
 fi
 
-if [ "${UPDATE_SYSTEM}"]; then
+if [ "${UPDATE_SYSTEM}" ]; then
     sudo apt-get update -y;
     sudo apt-get upgrade -y;
 fi
